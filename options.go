a457ad4aa1d6549fce750902031174a62c16297f
asfreng <34768829+asfreng@users.noreply.github.com>
2022-02-09 16:07:00 -0300

    Update options.go

--- options.go
+++ options.go
@@ -16,7 +16,7 @@ func (dsp *defaultSavePoint) Create(id string) string {
 	return fmt.Sprintf("SAVEPOINT %s", id)
 }
 func (dsp *defaultSavePoint) Release(id string) string {
-	return fmt.Sprintf("RELEASE SAVEPOINT %s", id)
+	return fmt.Sprintf("SAVEPOINT %s", id)
 }
 func (dsp *defaultSavePoint) Rollback(id string) string {
 	return fmt.Sprintf("ROLLBACK TO SAVEPOINT %s", id)
